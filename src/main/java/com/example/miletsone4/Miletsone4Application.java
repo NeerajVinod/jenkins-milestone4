6a822d7ce5bdb75c5e439c5781e2d3e29186cd5e
NeerajVinod <NeerajVinod>
2024-06-05 16:32:15 +0530

    neeraj7

---
+++ src/main/java/com/example/miletsone4/Miletsone4Application.java
@@ -0,0 +1,13 @@
+package com.example.miletsone4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miletsone4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miletsone4Application.class, args);
+	}
+
+}
